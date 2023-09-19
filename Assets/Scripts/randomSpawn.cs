355e3e149bc3a8fac1987a5ed725a83227495e03
Mudit jain <91784034+MJcheonma@users.noreply.github.com>
2023-09-19 23:38:55 +0530

    upload

---
+++ Assets/Scripts/randomSpawn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomSpawn : MonoBehaviour
+{
+    public GameObject objectprefab;
+
+    void Update()
+    {
+        
+    }
+}
